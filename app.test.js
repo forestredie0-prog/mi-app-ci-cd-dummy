6ff57577bbda8a882884d86ae2c1df086666a472
forestredie0-prog <forestredie0@gmail.com>
2025-11-18 18:32:07 -0600

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -3,3 +3,15 @@ const { sumar } = require('./app');
 test('Sumar 1 + 2 debe ser 3', () => {
     expect(sumar(1, 2)).toBe(3);
 });
+
+test('Sumar -1 + 1 debe ser 0', () => {
+    expect(sumar(-1, 1)).toBe(0);
+});
+
+test('Sumar 0 + 0 debe ser 0', () => {
+    expect(sumar(0, 0)).toBe(0);
+});
+
+test('Sumar 5 + 7 debe ser 12', () => {
+    expect(sumar(5, 7)).toBe(12);
+});
